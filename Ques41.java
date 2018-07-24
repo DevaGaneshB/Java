c34a3d8686a82af4d6a79e0963710a7639987c92
DevaGaneshB <40600219+DevaGaneshB@users.noreply.github.com>
2018-07-24 16:12:23 +0530

    Add files via upload

--- Ques41.java
+++ Ques41.java
@@ -1,11 +1,7 @@
-import java.util.Scanner;
 public class Ques41 {
 
-	public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-                System.out.println("Enter a letter");
-		char chr = sc.nextChar();
-		int chr1= chr;
-		System.out.println("The ASCII value  is :"+chr1);
+	public static void main(String[] String) {
+		int chr = 'Z';
+		System.out.println("The ASCII value of Z is :"+chr);
 	}
 }
